54d0ab56c226caba4e38b2783efb3e2d21caddf7
Jopie2016 <jpl212011@gmail.com>
2021-12-08 20:48:05 -0500

    adding remote backend

--- main.tf
+++ main.tf
@@ -8,6 +8,13 @@ terraform {
   required_version = "> 0.14"
 }
 
+backend "s3" {
+  bucket = "circle-ci-backend-20211209013943900700000001"
+  key    = "terraform/webapp/terraform.tfstate"
+  region = "us-east-1"
+}
+
+
 provider "aws" {
   region = var.region
 }
